955dfc0639e7aabb8e70dd6ab482ffd1831db93f
jinseong0407 <qkrwlstjd09@naver.com>
2024-08-12 15:13:49 +0900

    Replace project with new version

--- src/App.js
+++ src/App.js
@@ -1,56 +1,45 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './App.css'; // CSS 파일 import
-
-// 데스크탑 이미지 import
-import bann1 from './images/pic3.png';
-import bann2 from './images/pic1.png';
-import bann3 from './images/pic2.png';
-import exam1 from './images/exam1.png';
-import exam3 from './images/exam3.png';
-import fixedImage from './images/realbanner2.jpg';
-
-// 모바일 이미지 import (실제 파일 경로를 사용하세요)
-import bann1Mobile from './images/mo1.jpg';
-import bann2Mobile from './images/mo3.jpg';
-import bann3Mobile from './images/mo5.jpg';
-import exam1Mobile from './images/mo2.jpg';
-import exam3Mobile from './images/mo4.jpg';
-import fixedImageMobile from './images/moba1.jpg';
+import bb from './images/bb1.png'; // 예시 이미지 import
 
 function App() {
-  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
-
-  useEffect(() => {
-    function handleResize() {
-      setIsMobile(window.innerWidth < 768);
-    }
-
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
+  // handleDownload 함수 정의
+  const handleDownload = () => {
+    const link = document.createElement('a');
+    link.href = '/BoundAssetInstaller.exe'; // 다운로드할 .exe 파일의 경로
+    link.download = 'BoundAssetInstaller.exe'; // 다운로드될 파일의 이름
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
 
   return (
     <div className="App">
-      <div className="image-wrapper"><img src={isMobile ? bann1Mobile : bann1} alt="Banner 1" className="responsive" /></div>
-      <div className="image-wrapper"><img src={isMobile ? exam1Mobile : exam1} alt="Example 1" className="responsive" /></div>
-      <div className="image-wrapper"><img src={isMobile ? bann2Mobile : bann2} alt="Banner 2" className="responsive" /></div>
-
-      <div className="relative-container">
-        <div className="image-wrapper"><img src={isMobile ? exam3Mobile : exam3} alt="Example 3" className="responsive" /></div>
-        <a href="https://open.kakao.com/o/gTC25oac" target="_blank" rel="noopener noreferrer" className="relative-link" aria-label="Link to Example 3"></a>
+      <div className="image-wrapper">
+        <img src={bb} alt="Banner" className="responsive" />
       </div>
+      <div className="text-wrapper">
+        <h1 className="title">🎉🌟Bound 8월 여름 이벤트🌟🎉</h1>
+        <p className="description">
+          이달의 신규 이벤트에 참여하여 다양한 혜택을 누리세요!<br />
+          <br />
+          <span className="highlight">👍 초특가 수수료 이벤트!</span><br />
+          최저 수수료 $3 적용! 업계 최저가를 보장합니다!<br />
+          <br />
+          <span className="highlight">💸 수수료 페이백 프로모션!</span><br />
+          매주 발생하는 수수료의 50%를 환급해드립니다!<br />
+          <br />
+          <span className="emphasis">🎁 신규 가입 혜택!</span><br />
+          신규 가입하시는 모든 분께 즉시 300,000원의 증거금을 지원해 드립니다!<br />
+          <br />
 
-      <div className="image-wrapper"><img src={isMobile ? bann3Mobile : bann3} alt="Banner 3" className="responsive" /></div>
-
-      <div className="fixed-container">
-        <div className="image-wrapper"><img src={isMobile ? fixedImageMobile : fixedImage} alt="Fixed Banner" className="fixed" /></div>
-        <a href="https://open.kakao.com/o/gTC25oac" target="_blank" rel="noopener noreferrer" className="fixed-link" aria-label="Link to Kakao Open Chat"></a>
+          🔥 이 모든 혜택을 누리려면 지금 바로 가입하세요!<br />
+        </p>
+        <button onClick={handleDownload} className="download-button">다운로드</button>
       </div>
-      
-      <div className="footer-padding"></div>
-     
     </div>
   );
 }
 
+// App 컴포넌트를 default export로 내보냄
 export default App;
